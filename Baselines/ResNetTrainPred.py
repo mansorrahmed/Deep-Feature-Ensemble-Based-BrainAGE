0209d11a0a3e3168a748074a804a7c58c51a0d19
mansoorbalouch <mansoorbaloch931@gmail.com>
2023-09-29 10:57:25 +0500

    modified:   Baselines/ResNetTrainPred.py

--- Baselines/ResNetTrainPred.py
+++ Baselines/ResNetTrainPred.py
@@ -31,7 +31,7 @@ class Trainer:
         self.model_dir = model_dir
         self.criterion = criterion
         self.optimizer = optimizer
-        self.sfcnModel = Model
+        self.resnetModel = Model
         self.prevEpochs = 0
 
 
@@ -40,31 +40,18 @@ class Trainer:
         - loads the dataset and creates data-loaders for the training and test set
         """
         print("Started loading data..")
-        X_train = torch.from_numpy(np.load(source_path + "X_train_VBM_OpenBHB.npy")).permute(0,4,1,2,3)
-        Y_train = np.load(source_path + "Y_train_VBM_OpenBHB.npy")
+        self.X_train = torch.from_numpy(np.load(source_path + "X_train_VBM_OpenBHB.npy")).permute(0,4,1,2,3)
+        self.Y_train = np.load(source_path + "Y_train_VBM_OpenBHB.npy")
 
-        X_test = torch.from_numpy(np.load(source_path + "X_test_VBM_OpenBHB.npy")).permute(0,4,1,2,3)
-        Y_test = np.load(source_path + "Y_test_VBM_OpenBHB.npy")
+        self.X_test = torch.from_numpy(np.load(source_path + "X_test_VBM_OpenBHB.npy")).permute(0,4,1,2,3)
+        self.Y_test = np.load(source_path + "Y_test_VBM_OpenBHB.npy")
 
-        # Transforming the age to soft label (probability distribution)
-        bin_range = [5,88]
-        bin_step = 1
-        sigma = 1
-        y_train, self.bc = dpu.num2vect(np.array(Y_train), bin_range, bin_step, sigma)
-        y_train = torch.tensor(y_train, dtype=torch.float32)
-
-
-        self.X_train = X_train
-        self.X_test = X_test
-        self.y_train = y_train
-        self.y_test = Y_test
-        
-        print("Data tensor loaded having shapes:> X-train: ", X_train.shape, 
-            "Y-train: ", y_train.shape)
+        print("Data tensor loaded having shapes:> X-train: ", self.X_train.shape, 
+            "Y-train: ", self.Y_train.shape)
    
-        self.train_dataloader = DataLoader(list(zip(X_train, y_train)), 
+        self.train_dataloader = DataLoader(list(zip(self.X_train, self.Y_train)), 
                                            batch_size=self.batchSize,  shuffle=True)
-        self.test_dataloader = DataLoader(list(zip(X_test, Y_test)), 
+        self.test_dataloader = DataLoader(list(zip(self.X_test, self.Y_test)), 
                                           batch_size=self.batchSize,  shuffle=True)
         print("Created train-test data loaders...")
 
@@ -81,7 +68,7 @@ class Trainer:
         prevEpochs = checkpoint['epoch']
         
         print("Model checkpoint loaded from Epoch: ", prevEpochs)
-        self.sfcnModel = model
+        self.resnetModel = model
         self.optimizer = optimizer
         self.nEpochs = self.nEpochs + prevEpochs
         self.prevEpochs = prevEpochs+1
@@ -127,7 +114,7 @@ class Trainer:
             X_batch = X_batch.to(device=self.device)
             y_batch = y_batch.to(device=self.device)
             
-            self.sfcnModel.train()
+            self.resnetModel.train()
             self.batch_loss = 0
             # loop through each sample in the current batch 
             for i in range(len(X_batch)):
@@ -135,12 +122,12 @@ class Trainer:
                 sample = sample.reshape((1,)+sample.shape)
 
                 # Forward propagation
-                output = self.sfcnModel(sample)
+                output = self.resnetModel(sample)
                 output = output[0].reshape([1, -1]).to(device=self.device)
                 # output is a tensor containing the soft labels (83 values) of the particular subject
 
                 # Compute the loss for each sample
-                loss = dpl.my_KLDivLoss(output, y_batch[i])
+                loss = criterion(output, y_batch[i])
 
                 self.batch_loss += loss  # Accumulate the loss for the entire batch
                 self.train_epoch_loss += loss  # Accumulate the loss for the entire epoch
@@ -157,9 +144,9 @@ class Trainer:
         train_epoch_avg_loss = round((self.train_epoch_loss / len(self.train_dataloader.dataset)).item(),3)
         print("Epoch:", self.epoch, "Loss:", train_epoch_avg_loss)
         # Save the model's state dictionary after each epoch
-        model_path = f'{self.model_dir}SFCN-Model-epoch-{self.epoch}.pth'
-        # torch.save(self.sfcnModel.state_dict(), model_path)
-        self.saveCheckpoint(self.sfcnModel,self.optimizer,model_path,
+        model_path = f'{self.model_dir}ResNet18-Model-epoch-{self.epoch}.pth'
+        # torch.save(self.resnetModel.state_dict(), model_path)
+        self.saveCheckpoint(self.resnetModel,self.optimizer,model_path,
                             self.epoch,train_epoch_avg_loss )
 
         return train_epoch_avg_loss
@@ -181,7 +168,7 @@ class Trainer:
         """
         self.test_epoch_mae = 0  
         print("Testing the model .." )
-        self.sfcnModel.eval()  # Set the model to evaluation mode
+        self.resnetModel.eval()  # Set the model to evaluation mode
         with torch.no_grad(): # we don't compute gradients here 
             # loop through each batch of the test set and compute the evaluation metrics
             for X_batch, y_batch in self.test_dataloader:
@@ -197,16 +184,13 @@ class Trainer:
                     sample = X_batch[i]
                     sample = sample.reshape((1,)+sample.shape)
                 
-                    output = self.sfcnModel(sample)
+                    output = self.resnetModel(sample)
                     output = output[0].reshape([1, -1])
 
                     x = output.detach().cpu().numpy().reshape(-1)
                     y = y_batch[i].detach().cpu().numpy().reshape(-1)
 
-                    prob = np.exp(x)
-                    pred = prob @ self.bc
-
-                    mae = torch.mean(torch.abs(pred - y_batch[i]))
+                    mae = torch.mean(torch.abs(output - y_batch[i]))
                     self.test_epoch_mae += mae.item() # Accumulate the loss for the entire epoch
                     self.batch_mae += mae.item() # Accumulate the loss for the entire batch
                     
